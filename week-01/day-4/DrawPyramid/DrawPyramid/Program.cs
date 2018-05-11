49aec584c209e38bb18a14f8d418abd1530ff963
Zoltan Kasza <zolkasza@gmail.com>
2018-05-11 12:31:24 +0200

    chore

---
+++ week-01/day-4/DrawPyramid/DrawPyramid/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DrawPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int spaces;
+
+            Console.WriteLine("Give me a number!");
+            number = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            spaces = (number / 2)-1;
+
+            for (int i = 1; i <= number; i+=2)
+            {
+                for (int j = spaces; j >= 0; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+                spaces -= 1;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-01/day-4/DrawTriangle/DrawTriangle/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DrawTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            Console.WriteLine("Give me a number!");
+            number = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 1; i <= number; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
