0afc06c5b048c9a185da873783182a31e830300e
Zoltan Kasza <zolkasza@gmail.com>
2018-05-11 12:59:48 +0200

    fix(pyramid.cs)

--- week-01/day-5/DrawPyramid/DrawPyramid/Program.cs
+++ week-01/day-5/DrawPyramid/DrawPyramid/Program.cs
@@ -12,19 +12,18 @@ namespace DrawPyramid
             Console.WriteLine("Give me a number!");
             number = int.Parse(Console.ReadLine());
             Console.WriteLine();
-            spaces = (number / 2)-1;
+            spaces = number-2;
 
-            for (int i = 1; i <= number; i+=2)
+            for (int i = 1; i <= number; i++)
             {
                 for (int j = spaces; j >= 0; j--)
                 {
                     Console.Write(" ");
                 }
-                for (int k = 1; k <= i; k++)
+                for (int k = 1; k <= (i*2)-1; k++)
                 {
                     Console.Write("*");
                 }
-                Console.WriteLine();
                 Console.WriteLine();
                 spaces -= 1;
             }
