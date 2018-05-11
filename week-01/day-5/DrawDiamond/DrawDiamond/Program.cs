4c8a7f05893bc15c774718aeda77efcdd4ce2594
Zoltan Kasza <zolkasza@gmail.com>
2018-05-11 15:35:33 +0200

    chore(diamond.cs)

---
+++ week-01/day-5/DrawDiamond/DrawDiamond/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace DrawDiamond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int spaces;
+
+            do
+            {
+                Console.WriteLine("Give me a number!");
+                number = int.Parse(Console.ReadLine());
+            } while (number%2 == 0);
+
+            Console.WriteLine();
+            spaces = number / 2;
+
+            for (int i = 1; i <= number; i++)
+            {              
+                if (i>=(number/2)+1)
+                {
+                    for (int j = 0; j <= i - ((number / 2) + 1); j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int k = ((number * 2) + 1) - (i * 2); k >=1; k--)
+                    {
+                        Console.Write("*");
+                    }
+                }
+                else 
+                {
+                    for (int j = spaces; j >= 0; j--)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int k = 1; k <= (i * 2) - 1; k++)
+                    {
+                        Console.Write("*");
+                    }
+                }
+                Console.WriteLine();
+                spaces -= 1;
+            }
+            Console.ReadLine();
+        }
+    }
+}
