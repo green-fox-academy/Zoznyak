f0bf0271c5ea72d288dcd2eb1532044235cd2918
Zoltan Kasza <zolkasza@gmail.com>
2018-05-14 14:37:14 +0200

    chore (SumElements.cs)

---
+++ week-02/day-1/SumElements/SumElements/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SumElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] r = { 54, 23, 66, 12 };
+            Console.WriteLine(Sum(r));
+            Console.ReadLine();
+        }
+        public static int Sum(int [] arrayOne)
+        {
+            return arrayOne[1] + arrayOne[2];
+        }
+    }
+}
