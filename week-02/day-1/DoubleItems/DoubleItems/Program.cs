4b4b8d9c03faaf599395b90205697b17a62ca777
Zoltan Kasza <zolkasza@gmail.com>
2018-05-14 15:49:29 +0200

    chore (DoubleItems.cs)

---
+++ week-02/day-1/DoubleItems/DoubleItems/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DoubleItems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numList = { 3, 4, 5, 6, 7 };
+
+            DoubleIt(numList);
+
+            for (int i = 0; i < numList.Length; i++)
+            {
+                Console.WriteLine(numList[i]);
+            }
+            Console.ReadLine();
+        }
+
+        public static int[] DoubleIt(int[] arrayOne)
+        {
+            for (int i = 0; i < arrayOne.Length; i++)
+            {
+                arrayOne[i] *= 2;
+            }
+            return arrayOne;
+        }
+    }
+}
