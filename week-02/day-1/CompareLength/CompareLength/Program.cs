0bf28e2814ec65bdc4495d7c0b543a80c19a7438
Zoltan Kasza <zolkasza@gmail.com>
2018-05-14 14:30:10 +0200

    chore (CompareLength.cs)

---
+++ week-02/day-1/CompareLength/CompareLength/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CompareLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] p1 = { 1, 2, 3 };
+            int[] p2 = { 4, 5, 7, 8 };
+            if (p2.Length>p1.Length)
+            {
+                Console.WriteLine("'p2' has more elements");
+            }
+            Console.ReadLine();
+        }
+    }
+}
