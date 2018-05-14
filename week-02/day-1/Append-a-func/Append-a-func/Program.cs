66b6b7be9b0e379fe7fddd148c8151fcbf52fa3e
Zoltan Kasza <zolkasza@gmail.com>
2018-05-14 12:51:16 +0200

    chore (append-a-func.cs)

---
+++ week-02/day-1/Append-a-func/Append-a-func/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Append_a_func
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string am = "Kuty";
+            Console.WriteLine(AppendA(am));
+            Console.ReadLine();
+        }
+        public static string AppendA(string text1)
+        {
+            return (text1 + "a");
+        }
+    }
+}
