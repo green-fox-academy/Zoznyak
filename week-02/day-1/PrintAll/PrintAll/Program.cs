162eed60b2cbf1ca4ae91f19f5e0f86cf3735634
Zoltan Kasza <zolkasza@gmail.com>
2018-05-14 14:47:26 +0200

    chore (PrintAll.cs)

---
+++ week-02/day-1/PrintAll/PrintAll/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PrintAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 4, 5, 6, 7 };
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
