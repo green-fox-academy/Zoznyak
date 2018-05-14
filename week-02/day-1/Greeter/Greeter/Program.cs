e7d5ecad302a0e220a5cd5868e92ac1af4b19709
Zoltan Kasza <zolkasza@gmail.com>
2018-05-14 12:48:56 +0200

    chore (greeter.cs)

---
+++ week-02/day-1/Greeter/Greeter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Greeter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string al = "Geenfox";
+            Console.WriteLine(Greet(al));
+            Console.ReadLine();
+        }
+        public static string Greet(string text1)
+        {
+            return ("Greetings deer, " + text1);
+        }
+    }
+}
