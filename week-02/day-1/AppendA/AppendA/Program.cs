b8c2a0b698ee23d3005748b09438c1d10f30d9c4
Zoltan Kasza <zolkasza@gmail.com>
2018-05-14 16:50:09 +0200

    chore (AppendA.cs)

---
+++ week-02/day-1/AppendA/AppendA/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AppendA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] animals = { "kuty", "macsk", "cic" };
+            ChangeIt(animals);
+            for (int i = 0; i < animals.Length; i++)
+            {
+                Console.WriteLine(animals[i]);
+            }           
+            Console.ReadLine();
+        }
+        public static string[] ChangeIt(string[] arrayOne)
+        {
+            for (int i = 0; i < arrayOne.Length; i++)
+            {
+                arrayOne[i] += "a";
+            }
+            return arrayOne;
+        }
+    }
+}
