a850e0ce2a108e38676981e5d3640ef523318a3b
Zoltan Kasza <zolkasza@gmail.com>
2018-05-14 16:59:39 +0200

    chore (SumAllElements.cs)

---
+++ week-02/day-1/SumAllElements/SumAllElements/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SumAllElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ai = { 3, 4, 5, 6, 7 };
+            Console.WriteLine(SumAll(ai));
+            Console.ReadLine();
+        }
+        public static int SumAll(int[] arrayOne)
+        {
+            int sum=0;
+            for (int i = 0; i < arrayOne.Length; i++)
+            {
+                sum += arrayOne[i];
+            }
+            return sum;
+        }
+    }
+}
