24d2ec3edf54ea17971b701e7c21dca50fdaa360
Zoltan Kasza <zolkasza@gmail.com>
2018-05-14 16:54:48 +0200

    chore (SwapElements.cs)

---
+++ week-02/day-1/SwapElements/SwapElements/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SwapElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] abc = { "first", "second", "third" };
+            SwapIt(abc);
+            for (int i = 0; i < abc.Length; i++)
+            {
+                Console.WriteLine(abc[i]);
+            }
+            Console.ReadLine();
+        }
+        public static string[] SwapIt(string[] arrayOne)
+        {
+            string help;
+            help = arrayOne[0];
+            arrayOne[0] = arrayOne[2];
+            arrayOne[2] = help;
+            return arrayOne;
+        }
+    }
+}
