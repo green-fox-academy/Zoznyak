3e39d6635192d952f1cbeaecdd7c35a122beda20
Zoltan Kasza <zolkasza@gmail.com>
2018-05-14 12:40:42 +0200

    chore (doubling.cs)

---
+++ week-02/day-1/Doubling/Doubling/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Doubling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int baseNum = 123;
+
+            Console.WriteLine(Doubling(baseNum));
+            Console.ReadLine();
+        }
+
+        static int Doubling(int num1)
+        {
+            return num1 * 2;
+        }
+    }
+}
