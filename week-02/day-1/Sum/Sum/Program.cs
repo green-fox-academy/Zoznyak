80256addd44a294f35c00d6145841f70ea24d607
Zoltan Kasza <zolkasza@gmail.com>
2018-05-14 14:03:50 +0200

    chore (Sum.cs)

---
+++ week-02/day-1/Sum/Sum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Give me a number");
+            number = int.Parse(Console.ReadLine());
+            Console.WriteLine(Sum(number));
+            Console.ReadLine();
+        }
+        public static int Sum(int num1)
+        {
+            return (num1 * (num1 + 1)) / 2;
+        }
+    }
+}
