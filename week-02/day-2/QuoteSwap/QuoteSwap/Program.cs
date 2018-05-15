95534a17e5ae4ba7dca6a447b44b85b74ddf07c6
Zoltan Kasza <zolkasza@gmail.com>
2018-05-15 21:46:48 +0200

    add (QuoteSwap): workshop

---
+++ week-02/day-2/QuoteSwap/QuoteSwap/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuoteSwap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<string> { "What", "I", "do", "create,", "I", "cannot", "not", "understand." };
+            Console.WriteLine(QuoteSwap(list,2,5));
+            Console.ReadLine();
+        }
+
+        public static string QuoteSwap(List<string> listOne,int index1,int index2)
+        {
+            string temp = "";
+            temp = listOne[index1];
+            listOne[index1] = listOne[index2];
+            listOne[index2] = temp;
+            return String.Join(" ", listOne.ToArray());
+        }
+    }
+}
