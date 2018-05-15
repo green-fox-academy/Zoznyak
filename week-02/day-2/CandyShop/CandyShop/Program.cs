57af1d54f52b4e3e36e2f5e001350cdee990d52f
Zoltan Kasza <zolkasza@gmail.com>
2018-05-15 17:38:00 +0200

    add (CandyShop): workshop

---
+++ week-02/day-2/CandyShop/CandyShop/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CandyShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<object>();
+            list.Add("Cupcake");
+            list.Add(2);
+            list.Add("Brownie");
+            list.Add(false);
+            Console.WriteLine(Sweet(list));
+            Console.ReadLine();
+        }
+        public static string Sweet(List<object> oneList)
+        {
+            var newList = new List<string>();
+            string correctListInString;
+            foreach (var item in oneList)
+            {
+                newList.Add(string.Format("{0} ", item));
+            }
+            correctListInString = String.Join(" ", newList.ToArray());
+            StringBuilder builder = new StringBuilder(correctListInString);
+            builder.Replace("2", "Croissont")
+                   .Replace("False", "Ice Cream");
+            return builder.ToString();
+        }
+    }
+}
