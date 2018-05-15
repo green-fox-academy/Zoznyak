bb1be9791e63290905171352dcbe43fcdff8af98
Zoltan Kasza <zolkasza@gmail.com>
2018-05-15 21:24:29 +0200

    add (IsItInList): workshop

---
+++ week-02/day-2/IsInList/IsInList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IsInList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list1 = new List<int> { 2, 4, 6, 8, 10, 12, 14, 16 };
+            var list2 = new List<int> { 4, 8, 12, 16 };
+            Console.WriteLine(IsItInList(list1,list2));
+            Console.ReadLine();
+        }
+
+        static public bool IsItInList(List<int> listOne, List<int> listTwo)
+        {
+            int matches = 0;
+            for (int i = 0; i < listTwo.Count; i++)
+            {
+                if (listOne.Contains(listTwo[i]))
+                {
+                    matches += 1;
+                }
+            }
+            return matches == listTwo.Count;
+        }
+    }
+}
