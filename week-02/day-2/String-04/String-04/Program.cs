57dc5ea861e843a25c29e457fd797f4989e929bc
Zoltan Kasza <zolkasza@gmail.com>
2018-05-15 12:31:06 +0200

    add (String-04): workshop

---
+++ week-02/day-2/String-04/String-04/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace String_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string toDoText = " - Buy milk\n";
+            StringBuilder newStringBuilder = new StringBuilder(toDoText);
+            newStringBuilder
+                .Insert(0, "My todo:\n")
+                .Append(" - Download games\n")
+                .Append("\t- Diablo");
+            Console.WriteLine(newStringBuilder);
+            Console.ReadLine();
+        }
+    }
+}
