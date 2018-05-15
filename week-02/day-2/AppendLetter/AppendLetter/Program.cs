3948a12fefed7d8788333a8a2839d6950d11b60b
Zoltan Kasza <zolkasza@gmail.com>
2018-05-15 15:32:47 +0200

    add (AppendLetter): workshop

---
+++ week-02/day-2/AppendLetter/AppendLetter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppendLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var far = new List<string> { "kuty", "macsk", "kacs", "rok", "halacsk" };
+            foreach (var word in AppendA(far))
+            {
+                Console.WriteLine(word);
+            }
+            Console.ReadLine();
+        }
+        public static List<string> AppendA(List<string> oneList)
+        {
+            var completeList = new List<string>();
+            string letter = "a";
+            foreach (var word in oneList)
+            {
+                completeList.Add(string.Format("{0}{1}", word, letter));
+            }
+            return completeList;
+        }
+    }
+}
