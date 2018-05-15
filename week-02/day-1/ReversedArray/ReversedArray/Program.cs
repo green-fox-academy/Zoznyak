358c5ee217868be292a842edf64eb053b7ea71a8
Zoltan Kasza <zolkasza@gmail.com>
2018-05-15 11:43:28 +0200

    add (ReversedArray, List-01): workshop

---
+++ week-02/day-1/ReversedArray/ReversedArray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ReversedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int[] abc = { 1, 2, 3, 4, 5 };
+                ReverseIt2(abc).CopyTo(abc, 0);
+                for (int i = 0; i < abc.Length; i++)
+                {
+                    Console.Write(abc[i] + " ");
+                }
+                Console.ReadLine();
+            }
+        }
+        public static int[] ReverseIt2(int[] arrayOne)
+        { 
+            {
+                int[] help = new int[arrayOne.Length];
+                for (int i = 0; i < arrayOne.Length; i++)
+                {
+                    help[(arrayOne.Length - i - 1)] = arrayOne[i];
+                }
+                return help;
+            }
+        }
+
+    }
+}
---
+++ week-02/day-2/Strings-ö1/Strings-ö1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Strings_ö1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string example = "In a dishwasher far far away";
+            example = example.Replace("dishwasher", "galaxy");
+            Console.WriteLine(example);
+            Console.ReadLine();
+        }
+    }
+}
