ff0cccf62b83ab2f42c8d18e702001b8a65d51c1
Zoltan Kasza <zolkasza@gmail.com>
2018-05-15 20:58:30 +0200

    add (ElementFinder): workshop

---
+++ week-02/day-2/ElementFinder/ElementFinder/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ElementFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int>{ 1, 2, 3, 4, 5 };
+            if (ContainsNumber(list) == true)
+            {
+                Console.WriteLine("Hoorraay");
+            }
+            else
+            {
+                Console.WriteLine("Nooooooo");
+            }
+            Console.ReadLine();
+        }
+        
+        public static bool ContainsNumber(List<int> oneList)
+        {
+            int number = 7;
+            return oneList.Contains(number);
+        }
+    }
+}
