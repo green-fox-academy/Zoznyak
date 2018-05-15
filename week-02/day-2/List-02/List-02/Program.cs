81df95e046ead3555ab3a96f676fa2537284cae6
Zoltan Kasza <zolkasza@gmail.com>
2018-05-15 11:50:52 +0200

    add (List-02): workshop

---
+++ week-02/day-2/List-02/List-02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace List_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https//www.reddit.com/r/nevertellmethebots";
+            url = CompleteHttps(url);
+            url = url.Replace("bots", "odds");
+            Console.WriteLine(url);
+            Console.ReadLine();
+        }
+
+        public static string CompleteHttps(string string1)
+        {
+            string1 = string1.Insert(5, ":");
+            return string1;
+        }
+    }
+}
