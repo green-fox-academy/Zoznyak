602967d52de13742951c45bc684679a6827c40ac
Zoltan Kasza <zolkasza@gmail.com>
2018-05-15 12:46:28 +0200

    add (String-05): workshop

---
+++ week-02/day-2/String-05/String-05/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace String_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textToReverse;
+            textToReverse = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+            textToReverse = ReverseString(textToReverse);
+            Console.WriteLine(textToReverse);
+            Console.ReadLine();
+        }
+        public static string ReverseString(string text)
+        {
+            string tempText = "";
+            for (int i = text.Length; i > 0; i--)
+            {
+                tempText += text[i-1];
+            }
+            return tempText;
+        }
+            
+    }
+}
