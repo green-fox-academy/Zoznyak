e502cbcc54a68a52303acb5f0a250290199e7f00
Zoltan Kasza <zolkasza@gmail.com>
2018-05-16 12:47:23 +0200

    add (CopyFile): workshop

---
+++ week-02/day-3/CopyFile/CopyFile/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace CopyFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file1Path = @"D:\Greenfox\Zoznyak\week-02\day-3\CopyFile\CopyFile\TextFile1.txt";
+            string file2Path = @"D:\Greenfox\Zoznyak\week-02\day-3\CopyFile\CopyFile\TextFile2.txt";
+            Console.WriteLine(CopyFiles(file1Path, file2Path));
+            Console.ReadLine();
+        }
+
+        public static bool CopyFiles(string file2, string file1)
+        {
+            try
+            {
+                File.WriteAllText(file1, File.ReadAllText(file2));
+                return true;
+            }
+            catch (Exception )
+            {
+                Console.WriteLine("Copy error");
+               return false;
+            }
+            
+        }
+    }
+}
