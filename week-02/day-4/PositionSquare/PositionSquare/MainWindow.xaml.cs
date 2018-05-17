0137e4cb1289d397c2141a541042f11bc0939261
Zoltan Kasza <zolkasza@gmail.com>
2018-05-17 16:06:08 +0200

    feat(PositionSquare): add DrawReactangle()

--- week-02/day-4/PositionSquare/PositionSquare/MainWindow.xaml.cs
+++ week-02/day-4/PositionSquare/PositionSquare/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Media;
 using FoxDrawNameSpace;
@@ -12,8 +13,21 @@ namespace PositionSquare
         {
             InitializeComponent();
             var foxDraw = new FoxDraw(canvas);
+            int startPosX = 0;
+            int startPosY = 0;
 
+            for (int i = 1; i < 4; i++)
+            {
+                DrawReactangle(startPosX, startPosY);
+                startPosX += 50;
+                startPosY += 50;
+            }         
+        }
+
+        private void DrawReactangle(int startPosX, int startPosY)
+        {
+            var foxDraw = new FoxDraw(canvas);
+            foxDraw.DrawRectangle(startPosX, startPosY, 50, 50);
         }
     }
-
 }
