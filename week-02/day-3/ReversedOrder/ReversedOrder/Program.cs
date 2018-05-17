2e87239a10030c89e263aae1bcb5692d4c80a4ef
Zoltan Kasza <zolkasza@gmail.com>
2018-05-17 12:05:06 +0200

    add (LineInTheMiddle-lines): workshop

---
+++ week-02/day-3/ReversedOrder/ReversedOrder/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Doubled
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file1 = @"D:\Greenfox\Zoznyak\week-02\day-3\ReversedOrder\ReversedOrder\reversed-order.txt";
+            string file2 = @"D:\Greenfox\Zoznyak\week-02\day-3\ReversedOrder\ReversedOrder\correct-order.txt";
+            ReversedOrder(file1, file2);
+            Console.ReadLine();
+        }
+
+        public static void ReversedOrder(string file1, string file2)
+        {
+            StreamReader reader = new StreamReader(file1);
+            string line = " ";
+            int numberOfLines = 0;
+            File.WriteAllText(file2, "");
+            while (line != null)
+            {
+                line = reader.ReadLine();
+
+                if (line != null)
+                {
+
+                    numberOfLines += 1;
+                }           
+            }
+            Console.WriteLine(numberOfLines);
+            for (int i = numberOfLines; i < numberOfLines; i++)
+            {
+
+            }
+        }
+    }
+}
---
+++ week-02/day-4/LineInTheMiddle/LineInTheMiddle/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+using FoxDrawNameSpace;
+
+namespace LineInTheMiddle
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            foxDraw.StrokeColor(Colors.Red);
+            foxDraw.DrawLine(0, canvas.Height/2, canvas.Width, canvas.Height/2);
+            foxDraw.StrokeColor(Colors.Green);
+            var startPoint = new Point(canvas.Width/2, 0);
+            var endPoint = new Point(canvas.Width/2, canvas.Height);
+            foxDraw.DrawLine(startPoint, endPoint);
+
+        }
+    }
+
+}
