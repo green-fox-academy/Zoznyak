bd2270da70959273b818570746b9c0ab28916155
Zoltan Kasza <zolkasza@gmail.com>
2018-05-17 14:24:36 +0200

    add (SquareInCenter): workshop

---
+++ week-02/day-4/SquareInCenter/SquareInCenter/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+using FoxDrawNameSpace;
+
+namespace SquareInCenter
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            foxDraw.StrokeColor(Colors.Green);
+            foxDraw.DrawLine(canvas.Width/2-5, canvas.Height/2-5, canvas.Width/2+5, canvas.Height/2-5);
+            foxDraw.DrawLine(canvas.Width/2+5, canvas.Height/2-5, canvas.Width/2+5, canvas.Height/2+5);
+            foxDraw.DrawLine(canvas.Width/2+5, canvas.Height/2+5, canvas.Width/2-5, canvas.Height/2+5);
+            foxDraw.DrawLine(canvas.Width/2-5, canvas.Height/2+5, canvas.Width/2-5, canvas.Height/2-5);
+
+        }
+    }
+}
