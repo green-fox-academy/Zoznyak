cf46b5344801f4b41bd3ab99f3a8830a1e2bdad2
Zoltan Kasza <zolkasza@gmail.com>
2018-05-17 17:05:34 +0200

    feat(RainbowBox): add DrawRainbowBoxes()

--- week-02/day-4/RainbowBox/RainbowBox/MainWindow.xaml.cs
+++ week-02/day-4/RainbowBox/RainbowBox/MainWindow.xaml.cs
@@ -10,10 +10,25 @@ namespace RainbowBox
     {
         public MainWindow()
         {
-            InitializeComponent();
-            var foxDraw = new FoxDraw(canvas);
+                InitializeComponent();
+                var foxDraw = new FoxDraw(canvas);
+                double sizeX = canvas.Width;
+                double sizeY = canvas.Height;
 
+                for (int i = 1; i < 11; i++)
+                {
+                    DrawRainbowBoxes(sizeX, sizeY);
+                    sizeX -= 50;
+                    sizeY -= 50;
+                }
+            }
+
+            private void DrawRainbowBoxes(double sizeX, double sizeY)
+            {
+                var foxDraw = new FoxDraw(canvas);
+                foxDraw.FillColor(Colors.Transparent);
+                foxDraw.DrawRectangle(canvas.Width / 2 - sizeX / 2, canvas.Height / 2 - sizeY / 2, sizeX, sizeY);
+            }
         }
-    }
 
 }
