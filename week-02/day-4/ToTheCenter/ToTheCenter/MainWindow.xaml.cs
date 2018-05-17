ed37d60a8a6ef9d93ba4e200398d946ee484b5f8
Zoltan Kasza <zolkasza@gmail.com>
2018-05-17 13:51:08 +0200

    add (ToTheCenter): workshop

---
+++ week-02/day-4/ToTheCenter/ToTheCenter/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+using FoxDrawNameSpace;
+
+namespace ToTheCenter
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            var startPoint = new Point();
+            for (int i = 0; i < 9; i+= 3)
+            {
+                startPoint.X = 100+i*30;
+                startPoint.Y = 200+i*30;
+                DrawLines(startPoint.X, startPoint.Y);
+            }
+        }
+
+        public void DrawLines(double num1, double num2)
+        {
+            var foxDraw = new FoxDraw(canvas);
+            var startPoint = new Point(num1,num2);
+            var endPoint = new Point(canvas.Width / 2, canvas.Height / 2);
+            foxDraw.DrawLine(startPoint, endPoint);
+        }
+    }
+}
