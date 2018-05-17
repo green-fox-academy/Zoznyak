2f83af51170e458b13c9745935c178c8675a7af9
Zoltan Kasza <zolkasza@gmail.com>
2018-05-17 12:47:34 +0200

    add (Diagonal): workshop

---
+++ week-02/day-4/Diagonal/Diagonal/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+using FoxDrawNameSpace;
+
+namespace Diagonal
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            var startpoint = new Point(0, 0);
+            var endpoint = new Point(canvas.Width, canvas.Height);
+            var startpoint2 = new Point(canvas.Width, 0);
+            var endpoint2 = new Point(0, canvas.Height);
+            foxDraw.StrokeColor(Colors.Green);
+            foxDraw.DrawLine(startpoint, endpoint);
+            foxDraw.StrokeColor(Colors.Red);
+            foxDraw.DrawLine(startpoint2, endpoint2);
+        }
+    }
+}
