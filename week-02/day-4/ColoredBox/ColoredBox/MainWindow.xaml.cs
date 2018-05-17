3efbd00ddd610e0b8cc2fd48fecdb3e7b5a5217c
Zoltan Kasza <zolkasza@gmail.com>
2018-05-17 12:25:14 +0200

    add (ColoredBox): workshop

---
+++ week-02/day-4/ColoredBox/ColoredBox/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+using FoxDrawNameSpace;
+
+namespace ColoredBox
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            foxDraw.StrokeColor(Colors.Green);
+            foxDraw.DrawLine(200, 100, 400,100);
+            foxDraw.StrokeColor(Colors.Red);
+            foxDraw.DrawLine(200, 300, 400, 300);
+            foxDraw.StrokeColor(Colors.Purple);
+            foxDraw.DrawLine(400, 100, 400, 300);
+            foxDraw.StrokeColor(Colors.Blue);
+            foxDraw.DrawLine(200, 300, 200, 100);
+        }
+    }
+
+}
