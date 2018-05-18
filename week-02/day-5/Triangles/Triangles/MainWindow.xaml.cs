bcbeb430ba99583369e930877d40fa929527aa77
Zoltan Kasza <zolkasza@gmail.com>
2018-05-18 10:49:20 +0200

    feat(Triangles): add DrawOneTriangle()

--- week-02/day-5/Triangles/Triangles/MainWindow.xaml.cs
+++ week-02/day-5/Triangles/Triangles/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Media;
 using FoxDrawNameSpace;
@@ -8,11 +9,27 @@ namespace Triangles
 
     public partial class MainWindow : Window
     {
+        FoxDraw foxDraw;
         public MainWindow()
         {
             InitializeComponent();
-            var foxDraw = new FoxDraw(canvas);
+            foxDraw = new FoxDraw(canvas);
+            var startPoint = new Point(200, 50);
+            double sizeOfTheTriangle = 40;
 
+            DrawOneTrianle(startPoint, sizeOfTheTriangle);
+        }
+
+        private void DrawOneTrianle(Point startPoint, double sizeOfTriangle)
+        {
+            double halfOfSide = sizeOfTriangle / 2;
+            double heightOfTriangle = Math.Sqrt(Math.Pow(sizeOfTriangle, 2) - Math.Pow(halfOfSide, 2));
+            var points = new List<Point>();
+            points.Add(new Point(startPoint.X, startPoint.Y));
+            points.Add(new Point(startPoint.X + halfOfSide, startPoint.Y + heightOfTriangle));
+            points.Add(new Point(startPoint.X - halfOfSide, startPoint.Y + heightOfTriangle));
+            foxDraw.FillColor(Colors.Transparent);
+            foxDraw.DrawPolygon(points);
         }
     }
 
