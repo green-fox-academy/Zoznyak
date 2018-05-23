1866116c1eddb70885304d6ef2e4e39bf74e76d9
Zoltan Kasza <zolkasza@gmail.com>
2018-05-23 12:03:45 +0200

    chore(PostIt)

---
+++ week-03/day-3/PostIt/PostIt/PostIt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PostIt
+{
+    class PostIt
+    {
+        public string backgroundColor;
+        public string textOnIt;
+        public string textColor;
+
+        public PostIt(string backgroundColor, string textOnIt, string textColor)
+        {
+            this.backgroundColor = backgroundColor;
+            this.textOnIt = textOnIt;
+            this.textColor = textColor;
+        }
+    }
+}
---
+++ week-03/day-3/PostIt/PostIt/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PostIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PostIt postItOne = new PostIt("Orange", "Idea 1", "Blue");
+            PostIt postItTwo = new PostIt("Pink", "Awsome", "Black");
+            PostIt postItThree = new PostIt("Green", "Superb!", "Green");
+
+            Console.WriteLine(postItOne.backgroundColor);
+            Console.WriteLine(postItThree.textOnIt);
+
+            Console.ReadLine();
+        }
+    }
+}
