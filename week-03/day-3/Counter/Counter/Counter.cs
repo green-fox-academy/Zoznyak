50f7a718b12196608d27d63c17844eea3c133fd9
Zoltan Kasza <zolkasza@gmail.com>
2018-05-23 14:51:38 +0200

    chore(Counter)

---
+++ week-03/day-3/Counter/Counter/Counter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Counter
+{
+    class Counter
+    {
+        private int defaultNumber;
+        public int result = 0;
+
+        public Counter(int defaultNumber)
+        {
+            this.defaultNumber = defaultNumber;
+            result = defaultNumber;
+        }
+
+        public int Add(int number)
+        {
+            result += number;
+            return result;
+        }
+
+        public int Add()
+        {
+            result++;
+            return result;
+        }
+
+        public int Get()
+        {
+            return result;
+        }
+
+        public int Reset()
+        {
+            result = this.defaultNumber;
+            return result;
+        }
+    }
+}
---
+++ week-03/day-3/Counter/Counter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter counterOne = new Counter(5);
+            counterOne.Add();
+            Console.WriteLine($"Current value is: {counterOne.Get()}");
+            counterOne.Add(7);
+            Console.WriteLine($"Current value is: {counterOne.Get()}");
+            counterOne.Reset();
+            Console.WriteLine($"Current value is: {counterOne.Get()}");
+            Console.ReadLine();
+        }
+    }
+}
