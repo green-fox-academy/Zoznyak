3c573ea444e7760427cd27d1c9ce15b76c59d1ef
Zoltan Kasza <zolkasza@gmail.com>
2018-05-24 12:46:27 +0200

    chore (Bunny1)

---
+++ week-03/day-4/Bunny1/Bunny1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Bunny1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BunnyEars(3));
+            Console.ReadLine();
+        }
+
+        public static int BunnyEars(int numberOfBunnys)
+        {
+            if (numberOfBunnys == 1)
+            {
+                return 2;
+            }
+
+            else
+            {
+                return 2 + BunnyEars(numberOfBunnys-1);
+            }
+        }
+    }
+}
