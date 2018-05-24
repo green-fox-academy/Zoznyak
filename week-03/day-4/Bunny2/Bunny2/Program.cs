04b36a08ae8e46e77df6d7c7e45c9eeb6ddbb9db
Zoltan Kasza <zolkasza@gmail.com>
2018-05-24 12:55:06 +0200

    chore (Bunny2)

---
+++ week-03/day-4/Bunny2/Bunny2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Bunny2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BunnyEars(5));
+            Console.ReadLine();
+        }
+
+        private static int BunnyEars(int numbersOfBunnies)
+        {
+            if (numbersOfBunnies == 1)
+            {
+                return 3;
+            }
+
+            else if (numbersOfBunnies % 2 == 0)
+            {
+                return 2 + BunnyEars(numbersOfBunnies - 1);
+            }
+
+            else
+            {
+                return 3 + BunnyEars(numbersOfBunnies - 1);
+            }
+        }
+    }
+}
