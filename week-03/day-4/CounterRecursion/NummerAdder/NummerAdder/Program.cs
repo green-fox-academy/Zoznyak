d58b9a7bcc83eb5ef231f5484e6a7568f7616822
Zoltan Kasza <zolkasza@gmail.com>
2018-05-24 11:22:55 +0200

    chore (RecursiveNummerAdder)

---
+++ week-03/day-4/CounterRecursion/NummerAdder/NummerAdder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NummerAdder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NummerAdder(6)); 
+            Console.ReadLine();
+        }
+
+        public static int NummerAdder(int n)
+        {
+            if (n == 1)
+            {
+                return 1;  
+            }
+            else
+            {
+                return n + NummerAdder(n - 1); 
+            }
+
+        }
+    }
+}
