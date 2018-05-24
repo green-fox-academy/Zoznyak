4434892bb29cd3c269ac74d6960f1e2a8f8d85c4
Zoltan Kasza <zolkasza@gmail.com>
2018-05-24 11:04:54 +0200

    chore (RecursiveCounter)

---
+++ week-03/day-4/CounterRecursion/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RecursiveCounter(8);
+            Console.ReadLine();
+        }
+
+        public static void RecursiveCounter(int n)
+        {
+            if (n >= 0)
+            {
+                Console.WriteLine(n);
+                RecursiveCounter(n-1);
+            }            
+        }
+    }
+}
