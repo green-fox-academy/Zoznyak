d667b1bae4d577b4cf376e6473bb5521009f9363
Zoltan Kasza <zolkasza@gmail.com>
2018-05-24 12:30:41 +0200

    chore (Power)

---
+++ week-03/day-4/Power/Power/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PowerIt(3, 1));
+            Console.ReadLine();
+        }
+
+        private static int PowerIt(int number, int power)
+        {
+            if (power == 0)
+            {
+                return 1;
+            }
+
+            else if (power == 1)
+            {
+                return number;
+            }
+
+            return number * PowerIt(number, power - 1);
+        }
+    }
+}
