438445d6b4f3dc47a91a7fa4042319249e0dfc3d
Zoltan Kasza <zolkasza@gmail.com>
2018-05-24 11:49:00 +0200

    chore (RecursiveSumDigits)

---
+++ week-03/day-4/CounterRecursion/SumDigit/SumDigit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SumDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumDigits(45));
+            Console.ReadLine();
+        }
+
+        private static int SumDigits(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return n % 10 + SumDigits(n / 10);
+            }
+        }
+    }
+}
