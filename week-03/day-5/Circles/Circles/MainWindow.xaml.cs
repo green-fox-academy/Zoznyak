3c04fedfd5c9159a80a76664e8ae0e9fbf2c76de
Zoltan Kasza <zolkasza@gmail.com>
2018-05-25 10:56:50 +0200

    chore (Circles)

---
+++ week-03/day-5/Circles/Circles/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+using FoxDrawNameSpace;
+
+namespace Circles
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            double circleSize = 100;
+            double centerX = canvas.Width / 2;
+            double centerY = canvas.Height / 2;
+
+            foxDraw.StrokeColor(Colors.Black);
+            foxDraw.FillColor(Colors.Transparent);
+
+
+
+            foxDraw.DrawEllipse(centerX, centerY, circleSize, circleSize);
+            circleSize = circleSize / 2;
+
+
+
+        }
+    }
+
+}
