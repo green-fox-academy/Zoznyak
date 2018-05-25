048766f5ecf2efb3cf4ca41bcecd649e44d528e9
Zoltan Kasza <zolkasza@gmail.com>
2018-05-25 12:38:05 +0200

    feat(Circles) add: centralpointtostart

--- week-03/day-5/Circles/Circles/MainWindow.xaml.cs
+++ week-03/day-5/Circles/Circles/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Media;
 using FoxDrawNameSpace;
@@ -12,20 +13,38 @@ namespace Circles
         {
             InitializeComponent();
             var foxDraw = new FoxDraw(canvas);
-            double circleSize = 100;
-            double centerX = canvas.Width / 2;
-            double centerY = canvas.Height / 2;
+            double circleSize = 200;
+            double centralX = canvas.Width / 2;
+            double centralY = canvas.Height / 2;
+            Point startPoint = CentralPointToStart(centralX, centralY, circleSize);
 
             foxDraw.StrokeColor(Colors.Black);
             foxDraw.FillColor(Colors.Transparent);
 
-
-
-            foxDraw.DrawEllipse(centerX, centerY, circleSize, circleSize);
+            foxDraw.DrawEllipse(startPoint.X, startPoint.Y, circleSize, circleSize);
+            
             circleSize = circleSize / 2;
+            double circleRadius = circleSize / 2;
+
+            centralX += Math.Cos(DegreeToRadian(30)) * circleRadius;
+            centralY += Math.Sin(DegreeToRadian(30)) * circleRadius;
+            startPoint = CentralPointToStart(centralX, centralY, circleSize);
+            foxDraw.DrawEllipse(startPoint.X, startPoint.Y, circleSize, circleSize);
 
 
 
+
+        }
+
+        private double DegreeToRadian(double angle)
+        {
+            return Math.PI * angle / 180.0;
+        }
+
+        public Point CentralPointToStart(double centralX, double centralY, double size)
+        {
+            Point startPoint = new Point(centralX-(size/2), centralY-(size/2));
+            return startPoint;
         }
     }
 
