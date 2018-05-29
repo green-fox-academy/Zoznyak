1acc30d0f880d5d0b99a4ebe43eafa70179aacc1
Zoltan Kasza <zolkasza@gmail.com>
2018-05-29 12:52:35 +0200

    chore (GreenFox)

---
+++ week-04/day-2/GreenFox/GreenFox/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreenFox
+{
+    class Person
+    {
+        private string name;
+        private int age;
+        private string gender;
+
+        public Person(string name, int age, string gender)
+        {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+        }
+
+
+    }
+}
