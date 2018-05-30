cc0c85ba5c4146e5a6ab46c55fd9e109c9a2f6fd
Zoltan Kasza <zolkasza@gmail.com>
2018-05-30 09:02:08 +0200

    (String1)

---
+++ week-03/day-4/String1/String1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace String1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ChangeString("xhxhxh"));
+            Console.ReadLine();
+        }
+        //not done
+
+        public static string ChangeString(string stringArg)
+        {
+            char temp = stringArg[0];
+            if (temp == 'x')
+            {
+                temp = 'y';
+            }
+            return stringArg
+        }
+    }
+}
