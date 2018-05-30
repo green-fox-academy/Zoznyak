9d0a79f32755a7bdc2828fdedbd32ae5a6d6ab40
Zoltan Kasza <zolkasza@gmail.com>
2018-05-30 15:34:38 +0200

    chore (Zoo)

---
+++ week-04/day-3/Zoo/Zoo/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+    public abstract class Animal
+    {
+        private string name;
+        private int age;
+        private string gender;
+        private int weight;
+
+        public abstract string WantChild();
+        public abstract string Greeet();
+    }
+}
