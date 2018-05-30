8f0c0e0f0f55e7aa7b7a511700dbfccb51c80cd1
Zoltan Kasza <zolkasza@gmail.com>
2018-05-30 11:47:34 +0200

    chore (Instruments)

---
+++ week-04/day-3/Instruments/Instruments/Instrument.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Instruments
+{
+    public abstract class Instrument
+    {
+        private string name;
+
+        protected Instrument(string name)
+        {
+            this.name = name;
+        }
+
+        public abstract void Play();
+    }
+}
