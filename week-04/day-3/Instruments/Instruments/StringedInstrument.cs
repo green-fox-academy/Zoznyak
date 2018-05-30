fe60c1efbf6e4692056428a5af31f7fcac9dfb07
Zoltan Kasza <zolkasza@gmail.com>
2018-05-30 12:02:42 +0200

    feat (Instruments) add: Instrument  and StringedInstrument class

---
+++ week-04/day-3/Instruments/Instruments/StringedInstrument.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Instruments 
+{
+    class StringedInstrument : Instrument
+    {
+        private int numberOfStrings;
+
+        public StringedInstrument(int numberOfStrings) : base ()
+        {
+
+        }
+
+        public override void Play()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
