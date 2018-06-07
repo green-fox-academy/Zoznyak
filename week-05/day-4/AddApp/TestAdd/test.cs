5e5b539ef4278c7d931893f1702685afa25dbb7d
Zoltan Kasza <zolkasza@gmail.com>
2018-06-07 17:07:26 +0200

    chore(AddAppTest)

---
+++ week-05/day-4/AddApp/TestAdd/test.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddApp
+{
+    [TestFixture]
+    public class test
+    {
+        [TestCase]
+        public void Add()
+        {
+            Matek matek = new Matek();
+            Assert.AreEqual(6, matek.AddNumbers(2, 4));
+        }
+    }
+}
