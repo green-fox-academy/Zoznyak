71950b13ca2f9df50cf2396203c7de15ba9cba44
Zoltan Kasza <zolkasza@gmail.com>
2018-06-18 12:16:12 +0200

    chore (TwoNumbers)

---
+++ week-06/day-1/TwoNumbers/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1 = 22;
+    int num2 = 13;
+    int resultSubstraction;
+    int resultAddition;
+
+    resultAddition = num1 + num2;
+    resultSubstraction = num1 - num2;
+
+
+    printf("22 + 13 = %i\n", resultAddition);
+    printf("22 - 13 = %i\n", resultSubstraction);
+    return 0;
+}
+
