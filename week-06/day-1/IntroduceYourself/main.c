495bc9ffe5ddcbc0012877e9da76f8206ad73902
Zoltan Kasza <zolkasza@gmail.com>
2018-06-18 12:07:29 +0200

    add *.layout

---
+++ week-06/day-1/IntroduceYourself/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int age = 31;
+    char name[] = "Zoltan Kasza";
+    float height = 1.77;
+
+    printf("Hello, my name is: %s.\n", name);
+    printf("I'am %d years old and %.2f meter height.", age, height);
+    return 0;
+}
