adc6213df311f530cd0a92bbb04696e4eda966cc
Zoltan Kasza <zolkasza@gmail.com>
2018-06-18 14:31:41 +0200

    chore (DefineBasicInfo)

---
+++ week-06/day-1/DefineBasicInfo/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char firstLetter = 'Z';
+    int age = 31;
+    double height = 1.77;
+    unsigned int favoriteNumber = 23321;
+
+    printf("First letter of my name: %c\n", firstLetter);
+    printf("My age is: %i\n", age);
+    printf("My height is : %.2lf meter\n", height);
+    printf("My favorite number is: %i\n", favoriteNumber);
+    return 0;
+}
