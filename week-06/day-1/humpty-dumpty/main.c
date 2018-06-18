5151b735b7952995b9f57ed65dc8598562262939
Zoltan Kasza <zolkasza@gmail.com>
2018-06-18 11:45:43 +0200

    chore(HumptyDumpty)

---
+++ week-06/day-1/humpty-dumpty/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Humpty Dumpty sat on a wall,\n");
+    printf("Humpty Dumpty had a great fall.\n");
+    printf("All the king's horses and all the king's men\n");
+    printf("Couldn't put Humpty together again.");
+    return 0;
+}
