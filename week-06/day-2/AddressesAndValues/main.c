5657c052c940b416f762aa4cf0f4642b1fed4b6c
Zoltan Kasza <zolkasza@gmail.com>
2018-06-19 16:17:00 +0200

    feat (AddressesAndValues)add: Pi,letter

--- week-06/day-2/AddressesAndValues/main.c
+++ week-06/day-2/AddressesAndValues/main.c
@@ -12,5 +12,7 @@ int main()
 
     printf("Value\t Value with pointer\t Address\t Address with pointer\n");
     printf("%i\t\t %i\t\t%p\t\t%p\n", number, *pnumber, &number, pnumber);
+    printf("%.2f\t\t %.2f\t\t%p\t\t%p\n", Pi, *pPi, &Pi, pPi);
+    printf("%c\t\t %c\t\t%p\t\t%p\n", letter, *pletter, &letter, pletter);
     return 0;
 }
