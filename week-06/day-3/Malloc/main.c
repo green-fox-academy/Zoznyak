33d38dbb253b7dfa040c29bfb933ef728c1bc79f
Zoltan Kasza <zolkasza@gmail.com>
2018-06-20 14:59:08 +0200

    chore(Malloc)

---
+++ week-06/day-3/Malloc/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int *a = (int*)malloc(10*sizeof(int));
+    int i;
+
+    for (i = 0; i < 5; i++)
+    {
+        a[i] = i;
+    }
+    for (i = 0; i < 10; i++)
+    {
+        printf("%i\n", a[i]);
+    }
+    return 0;
+}
