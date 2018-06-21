b59008cd6ea80434491a7358ceb141a536dac6ba
Zoltan Kasza <zolkasza@gmail.com>
2018-06-21 23:27:12 +0200

    refactor (WriteMultipleLines)add: token_text()

--- week-06/day-4/WriteMultipleLines/main.c
+++ week-06/day-4/WriteMultipleLines/main.c
@@ -1,40 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void token_text(char line[], char *pword, char *ppath, int *number);
+
 int main()
 {
     char line[100];
     int number;
     char path[30];
     char word[30];
+    int *pnumber = &number;
+    char *ppath = &path;
+    char *pword = &word;
     printf("Enter a filename, a word and a number:\n");
-    gets(line);
-    //printf("%d", strlen(line));
+    token_text(gets(line), pword, ppath, pnumber);
+
+    return 0;
+}
+void token_text(char line[], char *pword, char *ppath, int *pnumber)
+{
     int i;
     char *p;
     for (p = strtok(line, " "); p != NULL; p = strtok(NULL, " "))
     {
         if (atoi(p) > 0 )
         {
-            number = atoi(p);
-            printf("Number: %d\n", number);
+            pnumber = atoi(p);
+            printf("Number: %d\n", pnumber);
         }
         if (strchr(p, '.') != NULL)
         {
-            strcpy(path, p);
-            printf("Path: %s\n", path);
+            strcpy(ppath, p);
+            printf("Path: %s\n", ppath);
         }
         else if (atoi(p) == 0)
         {
-            strcpy(word, p);
-            printf("Word: %s\n", word);
+            strcpy(pword, p);
+            printf("Word: %s\n", pword);
         }
     }
-/*
-    printf("%s\n", first);
-    printf("%s\n", second);
-    printf("%s\n", third);
-*/
-    return 0;
 }
 
