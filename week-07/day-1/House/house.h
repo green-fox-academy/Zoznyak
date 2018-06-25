7d889f44a65b8e0a9d4bd3cc4619547110afe182
Zoltan Kasza <zolkasza@gmail.com>
2018-06-25 20:34:02 +0200

    feat(House)add: worth_it()

--- week-07/day-1/House/house.h
+++ week-07/day-1/House/house.h
@@ -1,6 +1,13 @@
 #ifndef HOUSE_H_INCLUDED
 #define HOUSE_H_INCLUDED
+#define MARKETPRICE 400
 
+typedef struct{
+    char address[100];
+    float price;
+    int rooms;
+    float area;
+}t_house;
 
 
 #endif // HOUSE_H_INCLUDED
--- week-07/day-1/House/main.c
+++ week-07/day-1/House/main.c
@@ -2,8 +2,28 @@
 #include <stdlib.h>
 #include "house.h"
 
+void worth_it(t_house house);
+
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    t_house house1, house2;
+    strcpy(house1.address, "1027. Bp., Keletei Karoly 5.");
+    house1.area = 150;
+    house1.rooms = 3;
+    house1.price = 65000;
+    strcpy(house1.address, "1033. Bp., Bogdani ut 8.");
+    house2.area = 120;
+    house2.rooms = 3;
+    house2.price = 47000;
+
+    worth_it(house2);
+}
+void worth_it(t_house house)
+{
+    if (house.price / house.area < 400){
+        printf("It worths %.2lf Euro.\n", house.price);
+    }
+    else{
+        printf("It does not worth %.2lf Euro.\n", house.price);
+    }
 }
