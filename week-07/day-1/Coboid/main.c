d114fea7286cc1549bdc32b506baef60439fcbeb
Zoltan Kasza <zolkasza@gmail.com>
2018-06-25 19:30:49 +0200

    chore(Cuboid)

---
+++ week-07/day-1/Coboid/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "rectangle.h"
+
+// Write a function called "GetSurface" that takes a RectangularCuboid
+// and returns it's surface
+// Write a function called "GetVolume" that takes a RectangularCuboid
+// and returns it's volume
+
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
+
+
---
+++ week-07/day-1/Coboid/rectangle.h
@@ -0,0 +1,10 @@
+#ifndef RECTANGLE_H_INCLUDED
+#define RECTANGLE_H_INCLUDED
+
+struct rectangular_cuboid {
+    double a;
+    double b;
+    double c;
+};
+
+#endif // RECTANGLE_H_INCLUDED
