748285cb6935d01d6d946243d7c3a94acaf0e5c5
Zoltan Kasza <zolkasza@gmail.com>
2018-06-29 22:31:09 +0200

    feat(TempLogger)add: daily_avg()

--- week-07/day-4/TempLogger/average.c
+++ week-07/day-4/TempLogger/average.c
@@ -10,31 +10,69 @@
 get_daily_avg()
 {
 	char date[10];
+	int year;
+	int month;
+	int day;
 	do {
 	    clear_screen();
         printf("Please enter date (yyyy.mm.dd) you want to see the average temperature of!\n");
-        printf("\n");
         gets(date);
-	}while(valid_date(&date) != 1);
-	printf(" ");
+	}while(token_date(&date,&year,&month,&day) != 1);
+	printf("AVG: %.2fl",daily_avg(year, month, day));
 }
 
-int valid_date(char *date)
+int token_date(char *date, int *year, int *month, int *day)
 {
-    printf("%i", strlen(date));
     char *p;
-    int valid = 0;
+    int counter = 0;
     if(strlen(date) == 10){
         for (p = strtok(date, "."); p != NULL; p = strtok(NULL, ".")){
-            if(atoi(p) > 0 && atoi(p) < 2019){
-                valid++;
+            if(counter == 0){
+                if (check_year(atoi(p)) == 1){
+                    *year = atoi(p);
+                    counter++;
+                }
+            }
+            else if(counter == 1){
+                if (check_month(atoi(p)) == 1){
+                    *month = atoi(p);
+                    counter++;
+                }
+            }
+            else if(counter == 2){
+                if (check_day(atoi(p)) == 1){
+                    *day = atoi(p);
+                    counter++;
+                }
             }
         }
     }
-    if(valid == 3){
+    if(counter == 3){
         return 1;
     }
     else{
         return 0;
     }
+}
+
+float daily_avg(int year, int month, int day)
+{
+    int i = 0;
+    int match = 0;
+    int sum = 0;
+    float avg;
+    while ((1 + (int)log10(all_log[i].year)) == 4){
+        if(all_log[i].year == year && all_log[i].month == month && all_log[i].day == day){
+            sum += all_log[i].temperature;
+            match++;
+        }
+        i++;
+    }
+    if(match != 0){
+        avg = (float)sum / match;
+    }
+    else{
+        avg = 0;
+    }
+    return avg;
 }
--- week-07/day-4/TempLogger/average.h
+++ week-07/day-4/TempLogger/average.h
@@ -3,5 +3,7 @@
 
 get_daily_avg();
 int valid_date(char date[]);
+int token_date(char *date, int *year, int *month, int *day);
+float daily_avg(int year, int month, int day);
 
 #endif // AVERAGE_H_INCLUDED
